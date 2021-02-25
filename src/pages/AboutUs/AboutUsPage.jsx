21583da020c993a58a40232650eeb0f27612bbe3
Waidhoferj <jwaidhof@calpoly.edu>
2021-02-24 23:29:35 -0800

    added more styles

--- src/pages/AboutUs/AboutUsPage.jsx
+++ src/pages/AboutUs/AboutUsPage.jsx
@@ -1,14 +1,12 @@
 import "./AboutUsPage.scss";
 import TeamStory from "./sections/TeamStory/TeamStory";
 import Team from "./sections/Team/Team";
-import Advisors from "./sections/Advisors/Advisors";
 import Partner from "../../components/Partner/Partner";
 export default function AboutUsPage() {
   return (
     <article className="AboutUs">
       <TeamStory />
       <Team />
-      <Advisors />
       <Partner />
     </article>
   );

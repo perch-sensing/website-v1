a6c2599dcec4bed14a97b3b6c69051a9cd9ebcbb
Waidhoferj <jwaidhof@calpoly.edu>
2021-03-22 15:39:40 -0700

    basic route splitting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import { Switch, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import Lander from "./pages/Lander/LanderPage";
-import AboutUs from "./pages/AboutUs/AboutUsPage";
-import Contact from "./pages/Contact/ContactPage";
-import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import Navigation from "./components/Navigation/Navigation";
 import Footer from "./components/Footer/Footer";
+
+const Lander = lazy(() => import("./pages/Lander/LanderPage"));
+const AboutUs = lazy(() => import("./pages/AboutUs/AboutUsPage"));
+const Contact = lazy(() => import("./pages/Contact/ContactPage"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound/PageNotFound"));
 
 function App() {
   return (
     <main>
       <Navigation />
-      <Switch>
-        <Route path="/" exact component={Lander} />
-        <Route path="/about-us" component={AboutUs} />
-        <Route path="/contact" component={Contact} />
-        <Route path="*" component={PageNotFound} />
-      </Switch>
+      <Suspense fallback={<div></div>}>
+        <Switch>
+          <Route path="/" exact component={Lander} />
+          <Route path="/about-us" component={AboutUs} />
+          <Route path="/contact" component={Contact} />
+          <Route path="*" component={PageNotFound} />
+        </Switch>
+      </Suspense>
       <Footer />
     </main>
   );
